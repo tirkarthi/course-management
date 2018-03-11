a07829d962390d783b4970333eeb91d791be53b3
xtreak <tir.karthi@gmail.com>
2018-03-11 14:41:44 +0530

    Add model and migrations

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    limit = models.IntegerField(default=5)
+    students = models.ManyToManyField(User)
