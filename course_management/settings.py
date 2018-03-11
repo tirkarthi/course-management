10409ce5ad249857d1888e5f1b12fb920e99d761
xtreak <tir.karthi@gmail.com>
2018-03-11 14:44:39 +0530

    Disable DRF API browser and add urls

--- course_management/settings.py
+++ course_management/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
@@ -115,5 +117,7 @@ FIXTURE_DIRS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
